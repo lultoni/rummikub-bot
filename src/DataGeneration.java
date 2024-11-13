4987769e7bd603f04eb886d60fd62aaac005da31
lultoni <eeistcool@gmail.com>
2024-11-13 18:22:21 +0100

    Initial Commit

---
+++ src/DataGeneration.java
@@ -0,0 +1,14 @@
+public class DataGeneration {
+
+    // Track Number of the set
+    // Row or Group
+    // Legality (11 - Fully legal) (01 - Legal with 1/2 Additions)
+    // Which Type of Data does the number hold
+    //   (11 - Sequential Stones / Content)
+    //   (01 - Appendable Stones and the Side)
+    //   (10 - Splits and the sets that the split makes)
+
+    public static void main (String[] args) {
+
+    }
+}
