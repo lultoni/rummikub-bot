fdaa69b7237c7b591d9ad243420a2d7aeeb759fb
lultoni <eeistcool@gmail.com>
2024-11-15 13:34:57 +0100

    new gen v02

--- src/DataGeneration.java
+++ src/DataGeneration.java
@@ -49,32 +49,73 @@ public class DataGeneration {
             String[] colors = {"b", "s", "r", "y"};
 
             // TODO rows(blue, black, red, yellow)
-            // one per set
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = 0; j < 4; j++) {
-                    String col = colors[j];
-                    String addition = setID + isRow + isPartLegal; // base
-                    addition += typeContentDiv + col + getNumStr(nums[i]); // content
-                    addition += typeAppendDiv + col + getNumStr(getPrevNum(nums[i])) + col + getNumStr(getNextNum(nums[i])); // append
-                    addition += typeSplitDiv + "-";
-                    results.add(addition);
+            for (String color : colors) {
+                for (int i = 0; i < nums.length; i++) {
+                    for (int length = 1; length <= 14 - nums[i]; length++) {
+                        String legality = (length >= 3) ? isFullyLegal : isPartLegal;
+                        StringBuilder addition = new StringBuilder();
+                        addition.append(setID).append(isRow).append(legality);
+                        addition.append(typeContentDiv);
+                        for (int k = 0; k < length; k++) {
+                            addition.append(color).append(getNumStr(nums[(i + k) % nums.length]));
+                        }
+                        // Appendable stones
+                        addition.append(typeAppendDiv);
+                        if (length == 12) {
+                            // Only the remaining single stone not included
+                            for (int n : nums) {
+                                boolean isInSet = false;
+                                for (int k = 0; k < length; k++) {
+                                    if (nums[(i + k) % nums.length] == n) {
+                                        isInSet = true;
+                                        break;
+                                    }
+                                }
+                                if (!isInSet) {
+                                    addition.append(color).append(getNumStr(n));
+                                    break;
+                                }
+                            }
+                        } else if (length == 13) {
+                            // No stone can be appended
+                            addition.append("-");
+                        } else {
+                            addition.append(color).append(getNumStr(getPrevNum(nums[i])));
+                            addition.append(color).append(getNumStr(getNextNum(nums[(i + length - 1) % nums.length])));
+                        }
+                        addition.append(typeSplitDiv).append("-");
+
+                        results.add(addition.toString());
+                        tickID();
+                    }
+                }
+            }
+            // TODO groups(1-13)
+            for (int num : nums) {
+                for (int length = 1; length <= 4; length++) {
+                    String legality = (length >= 3) ? isFullyLegal : isPartLegal;
+                    StringBuilder addition = new StringBuilder();
+                    addition.append(setID).append(isGroup).append(legality);
+                    addition.append(typeContentDiv);
+                    for (int k = 0; k < length; k++) {
+                        addition.append(colors[k]).append(getNumStr(num));
+                    }
+                    // Appendable stones
+                    addition.append(typeAppendDiv);
+                    if (length == 4) {
+                        // No stone can be appended
+                        addition.append("-");
+                    } else {
+                        for (int k = length; k < colors.length; k++) {
+                            addition.append(colors[k]).append(getNumStr(num));
+                        }
+                    }
+                    addition.append(typeSplitDiv).append("-");
+
+                    results.add(addition.toString());
                     tickID();
                 }
-
             }
-            // two per set
-            // three per set
-            // four per set
-            // five
-            // six
-            // seven
-            // eight
-            // nine
-            // ten
-            // eleven
-            // twelve
-            // thirteen
-            // TODO groups(1-13)
             // TODO run through results - exchange each of the pieces for one joker
             // TODO run through results - exchange second non-joker with joker
 
