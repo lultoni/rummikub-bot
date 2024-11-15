c9f7e50723120294002ed2e367c84c318ab4de57
lultoni <eeistcool@gmail.com>
2024-11-15 12:50:00 +0100

    new gen v01

--- src/DataGeneration.java
+++ src/DataGeneration.java
@@ -1,7 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 public class DataGeneration {
 
@@ -43,62 +43,66 @@ public class DataGeneration {
 
     private static void generateSets() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath, true))) {
-            boolean unfinished = true;
-            while (unfinished) {
-                String result = generateSet(); // TODO find a way to not gen them one by one but grouped together -> which groups exist in the first
-                // TODO rows(blue, black, red, yellow) groups(1-13)
-                // TODO !!! per row and group, exchange each of the pieces for one joker
-                // TODO !!! !!! per each joker containing group, exchange second non-joker with joker
-                if (result != null) {
-                    writer.write(result + System.lineSeparator());
+            ArrayList<String> results = new ArrayList<>();
+
+            int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+            String[] colors = {"b", "s", "r", "y"};
+
+            // TODO rows(blue, black, red, yellow)
+            // one per set
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = 0; j < 4; j++) {
+                    String col = colors[j];
+                    String addition = setID + isRow + isPartLegal; // base
+                    addition += typeContentDiv + col + getNumStr(nums[i]); // content
+                    addition += typeAppendDiv + col + getNumStr(getPrevNum(nums[i])) + col + getNumStr(getNextNum(nums[i])); // append
+                    addition += typeSplitDiv + "-";
+                    results.add(addition);
                     tickID();
                 }
 
-                if (setID >= 1000) {  // Limit set count for demonstration
-                    unfinished = false;
-                }
+            }
+            // two per set
+            // three per set
+            // four per set
+            // five
+            // six
+            // seven
+            // eight
+            // nine
+            // ten
+            // eleven
+            // twelve
+            // thirteen
+            // TODO groups(1-13)
+            // TODO run through results - exchange each of the pieces for one joker
+            // TODO run through results - exchange second non-joker with joker
+
+            for (String res: results) {
+                writer.write(res + System.lineSeparator());
+                tickID();
             }
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file: " + e.getMessage());
         }
     }
 
-    private static String generateSet() { // TODO generate a single set?
-        ArrayList<String> setData = new ArrayList<>();
-
-        // Example attributes for a Row set
-        setData.add(formatSetID(setID));
-        setData.add(Integer.toBinaryString(isRow));
-        setData.add(Integer.toBinaryString(isFullyLegal));
-        setData.add(Integer.toBinaryString(typeContent));
-        setData.add(generateStoneSequence());
-
-        // Appendable stones
-        setData.add(generateAppendableStones());
-
-        // Splittable index data
-        setData.add(generateSplitData());
-
-        return String.join("", setData);
+    private static String getNumStr (int number) {
+        return number < 10 ? "0" + number : "" + number;
     }
 
-    private static String generateStoneSequence() {
-        // Assume a simple sequence for demonstration, e.g., "011100" for a row set
-        // This should encode stone color and number (e.g., Blue 1, Blue 2, Blue 3)
-        return "(011100)"; // TODO check stone list
+    private static int getPrevNum (int number) {
+        number--;
+        return number <= 0 ? 13 : number;
     }
 
-    private static String generateAppendableStones() {
-        // Return binary-encoded data for appendable stones and side (left or right)
-        return "(000000)"; // TODO check stone list
-    }
-
-    private static String generateSplitData() {
-        // Generate split information for this set, if applicable
-        return "(00110)(0)(000000)"; // TODO check stone list
+    private static int getNextNum (int number) {
+        number++;
+        return number > 13 ? 1 : number;
     }
 
     private static void tickID() {
         setID++;
     }
+
 }
