1f40a8bf996fc7458e735093867550fc93366805
lultoni <eeistcool@gmail.com>
2024-12-13 07:57:06 +0100

    joker in work

--- src/DataGeneration.java
+++ src/DataGeneration.java
@@ -20,14 +20,12 @@ public class DataGeneration {
     final static String isPartLegal = "01";
 
     // Which Type of Data does the number hold
-    // - (11 - Sequential Stones / Content) TODO i think the sequence does not matter, sorting will be done anyways
+    // - (11 - Sequential Stones / Content)
     final static String typeContentDiv = "$";
     // - (01 - Appendable Stones)
     final static String typeAppendDiv = "€";
     // - (10 - The sets that the split makes)
     final static String typeSplitDiv = "?";
-
-    // TODO structure of final line: 0R11$b08b09b10b11€b07b12?211?527
 
     public static void main(String[] args) {
         try {
@@ -49,13 +47,13 @@ public class DataGeneration {
             int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
             String[] colors = {"b", "s", "r", "y"};
 
-            // TODO rows(blue, black, red, yellow)
+            // rows(blue, black, red, yellow)
             for (String color : colors) {
                 for (int i = 0; i < nums.length; i++) {
                     for (int length = 1; length <= 14 - nums[i]; length++) {
                         String legality = (length >= 3) ? isFullyLegal : isPartLegal;
                         StringBuilder addition = new StringBuilder();
-                        addition.append(setID).append(length == 1 ? isSingle : isRow).append(legality);
+                        addition.append(getSetID()).append(length == 1 ? isSingle : isRow).append(legality);
                         addition.append(typeContentDiv);
                         for (int k = 0; k < length; k++) {
                             addition.append(color).append(getNumStr(nums[(i + k) % nums.length]));
@@ -109,19 +107,19 @@ public class DataGeneration {
                             addition.append(color).append(getNumStr(getPrevNum(nums[i])));
                             addition.append(color).append(getNumStr(getNextNum(nums[(i + length - 1) % nums.length])));
                         }
-                        addition.append(typeSplitDiv).append("-");
+                        addition.append(typeSplitDiv).append(generateSplits(addition));
 
                         results.add(addition.toString());
                         tickID();
                     }
                 }
             }
-            // TODO groups(1-13)
+            // groups(1-13)
             for (int num : nums) {
-                for (int length = 2; length <= 4; length++) {
+                for (int length = 2; length <= 4; length++) { // TODO this only give me b01 + whatever but not the other variations (find a way to not make dupes tho)
                     String legality = (length >= 3) ? isFullyLegal : isPartLegal;
                     StringBuilder addition = new StringBuilder();
-                    addition.append(setID).append(isGroup).append(legality);
+                    addition.append(getSetID()).append(isGroup).append(legality);
                     addition.append(typeContentDiv);
                     for (int k = 0; k < length; k++) {
                         addition.append(colors[k]).append(getNumStr(num));
@@ -136,15 +134,39 @@ public class DataGeneration {
                             addition.append(colors[k]).append(getNumStr(num));
                         }
                     }
-                    addition.append(typeSplitDiv).append("-");
+                    addition.append(typeSplitDiv).append(generateSplits(addition));
 
                     results.add(addition.toString());
                     tickID();
                 }
             }
-            // TODO run through results - exchange each of the pieces for one joker
-            // TODO run through results - exchange second non-joker with joker
+            // First Joker
+            ArrayList<String> joker_1_Results = new ArrayList<>();
+            for (int i = 0;  i < results.size(); i++) {
+                if ("S".equals(String.valueOf(results.get(i).charAt(4)))) {
+                    // if is Single -> do nothing?
+                    // System.out.println(results.get(i).substring(8, 12) + " - " + results.get(i).substring(0, 4));
+                } else {
+                    StringBuilder addition = new StringBuilder();
+                    addition.append(getSetID()).append(result_type).append(result_legality);
+                    addition.append(typeContentDiv);
+                    // TODO write the content here
+                    addition.append(typeAppendDiv);
+                    // TODO write appending stones here
+                    addition.append(typeSplitDiv).append(generateSplits(addition));
 
+                    joker_1_Results.add(addition.toString());
+                    tickID();
+                }
+
+            }
+            // Second Joker
+            ArrayList<String> joker_2_Results = new ArrayList<>();
+            // run through joker_1_Results
+            // replace a second non-joker with joker
+
+            results.addAll(joker_1_Results);
+            results.addAll(joker_2_Results);
             for (String res: results) {
                 writer.write(res + System.lineSeparator());
                 tickID();
@@ -152,6 +174,21 @@ public class DataGeneration {
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file: " + e.getMessage());
         }
+    }
+
+    private static String generateSplits(StringBuilder addition) {
+        // TODO from the addition work out into which sets you can split the set
+        // make a list of all the sublist combinations
+        // look for those sublist's in the possible sets and then put in that setID
+        // TODO how is the structure looking (sid, sid, div, ... || or '-' if no split is possible)
+        return "-";
+    }
+
+    private static String getSetID() {
+        if (setID >= 0 && setID < 10) return "000" + setID;
+        if (setID >= 10 && setID < 100) return "00" + setID;
+        if (setID >= 100 && setID < 1000) return "0" + setID;
+        return String.valueOf(setID);
     }
 
     private static String getNumStr (int number) {
